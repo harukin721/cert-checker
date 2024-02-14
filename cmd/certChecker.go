6987e6f038e1551c8a3e3d956b345384d672595d
harukin <harukin@pepabo.com>
2024-02-14 23:38:42 +0900

    最終行で OK,NG 数を出すようにした

--- cmd/certChecker.go
+++ cmd/certChecker.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"crypto/tls"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"time"
@@ -11,21 +12,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var okCount, ngCount int
+
 var certCheckerCmd = &cobra.Command{
 	Use:   "certChecker",
 	Short: "A CLI tool to check SSL certificate expiration",
 	Long:  `ssl_checker is a CLI tool that checks the expiration date of SSL certificates for a given list of URLs.`,
 
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			fmt.Println("Usage: certChecker <file>")
-			return
-		}
-		filePath := args[0]
-		file, err := os.Open(filePath)
+		file, err := os.Open(args[0])
 		if err != nil {
-			fmt.Printf("Error opening file: %v\n", err)
-			return
+			log.Fatalf("Failed to open file: %s", err)
 		}
 		defer file.Close()
 
@@ -34,15 +32,25 @@ var certCheckerCmd = &cobra.Command{
 			url := scanner.Text()
 			expirationDate, err := getExpirationDate(url)
 			if err != nil {
-				fmt.Printf("Error checking SSL certificate expiration for %s: %v\n", url, err)
+				log.Printf("Error checking SSL certificate expiration for %s: %s", url, err)
+				ngCount++
 				continue
 			}
-			fmt.Printf("SSL certificate for %s expires on: %s\n", url, expirationDate.Format("2006-01-02"))
+
+			log.Printf("SSL certificate for %s expires on: %s", url, expirationDate.Format("2006-01-02"))
+			if time.Now().After(expirationDate) {
+				ngCount++
+			} else {
+				okCount++
+			}
 		}
 
 		if err := scanner.Err(); err != nil {
-			fmt.Printf("Error reading file: %v\n", err)
+			log.Fatalf("Failed to read file: %s", err)
 		}
+
+		fmt.Println()
+		log.Printf("OK: %d, NG: %d", okCount, ngCount)
 	},
 }
 
